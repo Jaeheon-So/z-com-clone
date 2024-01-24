83c3d06b1264638e79fc90d7672c64936d7810ec
Jaeheon-So <sjh-98@naver.com>
2024-01-24 22:07:22 +0900

    feat: 로그인 페이지 리다이렉트 적용

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,6 +1,17 @@
-import { redirect } from "next/navigation";
-import React from "react";
+"use client";
 
-const Login = () => {};
+import { useRouter } from "next/navigation";
+import Main from "../_component/Main";
+import { useEffect } from "react";
+
+const Login = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/i/flow/login");
+  }, []);
+
+  return <Main />;
+};
 
 export default Login;
