d2618ee57b7651d85d707de1dc5bcf06278958f0
Jaeheon-So <sjh-98@naver.com>
2024-01-25 19:09:23 +0900

    feat: afterLogin layout nav 완성 및 svg 폴더 생성

---
+++ src/app/(afterLogin)/_component/NavMenu.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import style from "./navMenu.module.css";
+import { useSelectedLayoutSegment } from "next/navigation";
+import Link from "next/link";
+import HomeSvg from "../_svg/HomeSvg";
+import ExploreSvg from "../_svg/ExploreSvg";
+import MessageSvg from "../_svg/MessageSvg";
+import ProfileSvg from "../_svg/ProfileSvg";
+
+const NavMenu = () => {
+  const segment = useSelectedLayoutSegment();
+
+  const me = {
+    // 임시로 내 정보 있는것처럼
+    id: "zerohch0",
+  };
+
+  const navMenuData = [
+    {
+      id: "menu1",
+      name: "홈",
+      path: "/home",
+      segment: ["home"],
+      svg: <HomeSvg active={false} />,
+      activeSvg: <HomeSvg active={true} />,
+    },
+    {
+      id: "menu2",
+      name: "탐색하기",
+      path: "/explore",
+      segment: ["explore", "search"],
+      svg: <ExploreSvg active={false} />,
+      activeSvg: <ExploreSvg active={true} />,
+    },
+    {
+      id: "menu3",
+      name: "쪽지",
+      path: "/message",
+      segment: ["message"],
+      svg: <MessageSvg active={false} />,
+      activeSvg: <MessageSvg active={true} />,
+    },
+    {
+      id: "menu4",
+      name: "프로필",
+      path: `/${me.id}`,
+      segment: [me.id],
+      svg: <ProfileSvg active={false} />,
+      activeSvg: <ProfileSvg active={true} />,
+    },
+  ];
+
+  return (
+    <nav>
+      <ul>
+        {navMenuData.map((menu) =>
+          menu.path === `/${me.id}` ? (
+            me.id && (
+              <li key={menu.id}>
+                <Link href={menu.path}>
+                  <div className={style.navPill}>
+                    {menu.segment.includes(segment as string)
+                      ? menu.activeSvg
+                      : menu.svg}
+                    <div
+                      style={
+                        menu.segment.includes(segment as string)
+                          ? { fontWeight: "bold" }
+                          : {}
+                      }
+                    >
+                      {menu.name}
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            )
+          ) : (
+            <li key={menu.id}>
+              <Link href={menu.path}>
+                <div className={style.navPill}>
+                  {menu.segment.includes(segment as string)
+                    ? menu.activeSvg
+                    : menu.svg}
+                  <div
+                    style={
+                      menu.segment.includes(segment as string)
+                        ? { fontWeight: "bold" }
+                        : {}
+                    }
+                  >
+                    {menu.name}
+                  </div>
+                </div>
+              </Link>
+            </li>
+          )
+        )}
+      </ul>
+      <Link href="/compose/tweet" className={style.postButton}>
+        게시하기
+      </Link>
+    </nav>
+  );
+};
+
+export default NavMenu;
--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -1,16 +1,51 @@
 import { ReactNode } from "react";
+import style from "@/app/(afterLogin)/layout.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import zLogo from "../../../public/zlogo.png";
+import NavMenu from "./_component/NavMenu";
+import SearchSvg from "./_svg/SearchSvg";
 
 type Props = {
   children: ReactNode;
 };
 
-const AfterLoginLayout = ({ children }: Props) => {
-  return (
-    <div>
-      애프터 로그인 레이아웃
-      {children}
+const AfterLoginLayout = ({ children }: Props) => (
+  <div className={style.container}>
+    <header className={style.leftSectionWrapper}>
+      <section className={style.leftSection}>
+        <div className={style.leftSectionFixed}>
+          <Link className={style.logo} href="/home">
+            <div className={style.logoPill}>
+              <Image src={zLogo} alt="logo" width={40} height={40} />
+            </div>
+          </Link>
+          <NavMenu />
+          {/* <LogoutButton /> */}
+        </div>
+      </section>
+    </header>
+    <div className={style.rightSectionWrapper}>
+      <div className={style.rightSectionInner}>
+        <main className={style.main}>{children}</main>
+        <section className={style.rightSection}>
+          <div style={{ marginBottom: 60, width: "inherit" }}>
+            <form className={style.search}>
+              <SearchSvg />
+              <input type="search" />
+            </form>
+          </div>
+          {/* <TrendSection /> */}
+          <div className={style.followRecommend}>
+            <h3>팔로우 추천</h3>
+            {/* <FollowRecommend /> */}
+            {/* <FollowRecommend /> */}
+            {/* <FollowRecommend /> */}
+          </div>
+        </section>
+      </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default AfterLoginLayout;
