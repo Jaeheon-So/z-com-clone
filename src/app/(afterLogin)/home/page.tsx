e0881af2e5ccb6b9f188604a4a629cfbc05faab1
Jaeheon-So <sjh-98@naver.com>
2024-01-25 22:56:00 +0900

    feat: afterLogin home 페이지 게시물 마크업 진행(action button X)

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -3,6 +3,7 @@ import styles from "./home.module.css";
 import { TabProvider } from "./_component/TabProvider";
 import Tab from "./_component/Tab";
 import PostForm from "./_component/PostForm";
+import Post from "../_component/Post";
 
 const HomePage = () => {
   return (
@@ -11,18 +12,13 @@ const HomePage = () => {
       <TabProvider>
         <Tab />
         <PostForm />
-        {/* <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/>
-        <Post/> */}
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </TabProvider>
     </div>
   );
