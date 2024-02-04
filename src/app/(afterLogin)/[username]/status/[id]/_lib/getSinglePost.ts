5e25329978919ed755b2e3c4834b9e1d219b2f99
Jaeheon-So <sjh-98@naver.com>
2024-02-04 17:00:24 +0900

    feat: 개인 게시글 페이지 단일포스트, 답글 mock api 연결

---
+++ src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost.ts
@@ -0,0 +1,25 @@
+import { Post } from "@/model/Post";
+import { User } from "@/model/User";
+import { QueryFunction } from "@tanstack/react-query";
+
+export const getSinglePost: QueryFunction<
+  Post,
+  [_1: string, id: string]
+> = async ({ queryKey }) => {
+  const [_1, id] = queryKey;
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts/${id}`,
+    {
+      next: {
+        tags: ["posts", id],
+      },
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("failed to fetch data");
+  }
+
+  return res.json();
+};
--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -2,21 +2,46 @@ import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import style from "./singlePost.module.css";
 import Post from "@/app/(afterLogin)/_component/Post";
 import CommentForm from "./_component/CommentForm";
+import SinglePosts from "./_component/SinglePosts";
+import Comments from "./_component/Comments";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getSinglePost } from "./_lib/getSinglePost";
+import { getComments } from "./_lib/getComments";
 
-const SinglePostPage = () => {
+type Props = {
+  params: { id: string };
+};
+
+const SinglePostPage = async ({ params }: Props) => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", params.id],
+    queryFn: getSinglePost,
+  });
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", params.id, "comments"],
+    queryFn: getComments,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <div className={style.main}>
-      <div className={style.header}>
-        <BackButton />
-        <h3 className={style.headerTitle}>게시하기</h3>
-      </div>
-      <div className={style.marginTop}></div>
-      <Post />
-      <CommentForm />
-      <div>
-        <Post />
-        <Post />
-      </div>
+      <HydrationBoundary state={dehydratedState}>
+        <div className={style.header}>
+          <BackButton />
+          <h3 className={style.headerTitle}>게시하기</h3>
+        </div>
+        <div className={style.marginTop}></div>
+        <SinglePosts id={params.id} />
+        <CommentForm />
+        <div>
+          <Comments id={params.id} />
+        </div>
+      </HydrationBoundary>
     </div>
   );
 };
