fb98108819086b61c1043aab817edd65607e8c60
Jaeheon-So <sjh-98@naver.com>
2024-02-07 14:28:48 +0900

    feat: 게시물 이미지 업로드시 미리보기 추가

--- src/app/(afterLogin)/home/_component/PostForm.tsx
+++ src/app/(afterLogin)/home/_component/PostForm.tsx
@@ -4,6 +4,7 @@ import { ChangeEventHandler, FormEventHandler, useRef, useState } from "react";
 import style from "./postForm.module.css";
 import UploadSvg from "../../_svg/UploadSvg";
 import { Session } from "@auth/core/types";
+import TextareaAutosize from "react-textarea-autosize";
 
 type Props = {
   me: Session | null;
@@ -12,6 +13,9 @@ type Props = {
 const PostForm = ({ me }: Props) => {
   const imageRef = useRef<HTMLInputElement>(null);
   const [content, setContent] = useState("");
+  const [preview, setPreview] = useState<
+    Array<{ dataUrl: string; file: File } | null>
+  >([]);
 
   const onChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setContent(e.target.value);
@@ -20,6 +24,38 @@ const PostForm = ({ me }: Props) => {
   const onSubmit: FormEventHandler = (e) => {
     e.preventDefault();
     console.log("dsds");
+  };
+
+  const onUploadImage: ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+
+    if (e.target.files) {
+      if (e.target.files.length + preview.length > 4)
+        return alert("사진은 최대 4개까지 등록 가능합니다.");
+
+      Array.from(e.target.files).forEach((file, index) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setPreview((prevPreview) => {
+            const prev = [...prevPreview];
+            prev.push({
+              dataUrl: reader.result as string,
+              file,
+            });
+            return prev;
+          });
+        };
+        reader.readAsDataURL(file);
+      });
+    }
+  };
+
+  const onRemoveImage = (index: number) => () => {
+    setPreview((prevPreview) => {
+      const prev = [...prevPreview];
+      prev.splice(index, 1);
+      return prev;
+    });
   };
 
   const onClickButton = () => {
@@ -34,11 +70,33 @@ const PostForm = ({ me }: Props) => {
         </div>
       </div>
       <div className={style.postInputSection}>
-        <textarea
+        <TextareaAutosize
           value={content}
           onChange={onChange}
           placeholder="무슨 일이 일어나고 있나요?"
         />
+        <div style={{ display: "flex" }}>
+          {preview.map(
+            (v, index) =>
+              v && (
+                <div
+                  key={index}
+                  style={{ flex: 1, cursor: "pointer" }}
+                  onClick={onRemoveImage(index)}
+                >
+                  <img
+                    src={v.dataUrl}
+                    alt="미리보기"
+                    style={{
+                      width: "100%",
+                      objectFit: "contain",
+                      maxHeight: 100,
+                    }}
+                  />
+                </div>
+              )
+          )}
+        </div>
         <div className={style.postButtonSection}>
           <div className={style.footerButtons}>
             <div className={style.footerButtonLeft}>
@@ -48,6 +106,7 @@ const PostForm = ({ me }: Props) => {
                 multiple
                 hidden
                 ref={imageRef}
+                onChange={onUploadImage}
               />
               <button
                 className={style.uploadButton}
